09db6d7f4885191e62f29c2e90b4201d7c807afa
Rgamer43 <54045197+Rgamer43@users.noreply.github.com>
2021-03-29 16:14:14 -0400

    Edited ConnectionAccepter Built jar

--- src/main/java/ConnectionAccepter.java
+++ src/main/java/ConnectionAccepter.java
@@ -7,7 +7,7 @@ import java.net.*;
 public class ConnectionAccepter extends Thread{
     public void run() {
         try {
-            ServerSocket ss = new ServerSocket();
+            ServerSocket ss = new ServerSocket(Integer.valueOf(System.getenv("PORT")));
             System.out.println("PORT is " + System.getenv("PORT"));
             ss.bind(new InetSocketAddress(ss.getInetAddress(), Integer.valueOf(System.getenv("PORT"))));
 
